6b91dfdef5b2ba271a12551060efc6f4098194b2
taksas <takuminakayama4869@gmail.com>
2024-06-23 03:53:19 +0900

    Complications

--- app/src/main/java/net/taksas/apps/watchwaterejecter/complication/MainComplicationService.kt
+++ app/src/main/java/net/taksas/apps/watchwaterejecter/complication/MainComplicationService.kt
@@ -1,41 +1,100 @@
 package net.taksas.apps.watchwaterejecter.complication
 
+import android.app.PendingIntent
+import android.content.ContentValues.TAG
+import android.content.Intent
+import android.graphics.drawable.Icon
+import android.util.Log
 import androidx.wear.watchface.complications.data.ComplicationData
 import androidx.wear.watchface.complications.data.ComplicationType
+import androidx.wear.watchface.complications.data.LongTextComplicationData
+import androidx.wear.watchface.complications.data.MonochromaticImage
+import androidx.wear.watchface.complications.data.MonochromaticImageComplicationData
 import androidx.wear.watchface.complications.data.PlainComplicationText
 import androidx.wear.watchface.complications.data.ShortTextComplicationData
+import androidx.wear.watchface.complications.data.SmallImage
+import androidx.wear.watchface.complications.data.SmallImageComplicationData
+import androidx.wear.watchface.complications.data.SmallImageType
+import androidx.wear.watchface.complications.datasource.ComplicationDataSourceService
 import androidx.wear.watchface.complications.datasource.ComplicationRequest
 import androidx.wear.watchface.complications.datasource.SuspendingComplicationDataSourceService
+import net.taksas.apps.watchwaterejecter.R
+import net.taksas.apps.watchwaterejecter.presentation.EjectActivity
 import java.util.Calendar
 
 /**
  * Skeleton for complication data source that returns short text.
  */
 class MainComplicationService : SuspendingComplicationDataSourceService() {
-
     override fun getPreviewData(type: ComplicationType): ComplicationData? {
-        if (type != ComplicationType.SHORT_TEXT) {
-            return null
-        }
-        return createComplicationData("Mon", "Monday")
+        //if (type == ComplicationType.SHORT_TEXT) { return createShortTextComplicationData() }
+        //if (type == ComplicationType.LONG_TEXT) { return createLongTextComplicationData() }
+        if (type == ComplicationType.SMALL_IMAGE) { return createSmallImageComplicationData() }
+        //if (type == ComplicationType.SMALL_IMAGE) { return createMonochromaticImageComplicationData() }
+
+        return null
     }
 
-    override suspend fun onComplicationRequest(request: ComplicationRequest): ComplicationData {
-        return when (Calendar.getInstance().get(Calendar.DAY_OF_WEEK)) {
-            Calendar.SUNDAY -> createComplicationData("Sun", "Sunday")
-            Calendar.MONDAY -> createComplicationData("Mon", "Monday")
-            Calendar.TUESDAY -> createComplicationData("Tue", "Tuesday")
-            Calendar.WEDNESDAY -> createComplicationData("Wed", "Wednesday")
-            Calendar.THURSDAY -> createComplicationData("Thu", "Thursday")
-            Calendar.FRIDAY -> createComplicationData("Fri!", "Friday!")
-            Calendar.SATURDAY -> createComplicationData("Sat", "Saturday")
-            else -> throw IllegalArgumentException("too many days")
-        }
+    override suspend fun onComplicationRequest(request: ComplicationRequest): ComplicationData? {
+        //if (type == ComplicationType.SHORT_TEXT) { return createShortTextComplicationData() }
+        //if (type == ComplicationType.LONG_TEXT) { return createLongTextComplicationData() }
+        if (request.complicationType == ComplicationType.SMALL_IMAGE) { return createSmallImageComplicationData() }
+        //if (type == ComplicationType.SMALL_IMAGE) { return createMonochromaticImageComplicationData() }
+
+        return null
     }
 
-    private fun createComplicationData(text: String, contentDescription: String) =
-        ShortTextComplicationData.Builder(
-            text = PlainComplicationText.Builder(text).build(),
-            contentDescription = PlainComplicationText.Builder(contentDescription).build()
-        ).build()
+
+//    private fun createShortTextComplicationData() : ComplicationData {
+//        val intent = Intent(this, EjectActivity::class.java)
+//        val pendingIntent =
+//            PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+//        return ShortTextComplicationData.Builder(
+//            text = PlainComplicationText.Builder(getString(R.string.complication_description_short)).build(),
+//            contentDescription = PlainComplicationText.Builder(getString(R.string.complication_description))
+//                .build()
+//        ).setTapAction(pendingIntent).build()
+//
+//    }
+//
+//    private fun createLongTextComplicationData() : ComplicationData {
+//        val intent = Intent(this, EjectActivity::class.java)
+//        val pendingIntent =
+//            PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+//        return LongTextComplicationData.Builder(
+//            text = PlainComplicationText.Builder(getString(R.string.complication_description)).build(),
+//            contentDescription = PlainComplicationText.Builder(getString(R.string.complication_description))
+//                .build()
+//        ).setTapAction(pendingIntent).build()
+//
+//    }
+
+    private fun createSmallImageComplicationData() : ComplicationData {
+        val intent = Intent(this, EjectActivity::class.java)
+        val pendingIntent =
+            PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+        return SmallImageComplicationData.Builder(
+            smallImage = SmallImage.Builder(Icon.createWithResource(this, R.drawable.complication_icon), SmallImageType.ICON).build(),
+            contentDescription = PlainComplicationText.Builder(getString(R.string.complication_description))
+                .build()
+        ).setTapAction(pendingIntent).build()
+
+    }
+
+//    private fun createMonochromaticImageComplicationData(): ComplicationData {
+//        val intent = Intent(this, EjectActivity::class.java)
+//        val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+//        val icon = Icon.createWithResource(this, R.mipmap.ic_launcher)
+//        return MonochromaticImageComplicationData.Builder(
+//            monochromaticImage = MonochromaticImage.Builder(icon).build(),
+//            contentDescription = PlainComplicationText.Builder(getString(R.string.complication_description_short)).build()
+//        ).setTapAction(pendingIntent).build()
+//    }
+
+
+
+
 }
+
+
+
