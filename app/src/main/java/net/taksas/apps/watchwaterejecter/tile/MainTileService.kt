d9df11b8cc0bdb56e0b515d8c27f76f9f0ca3bb7
taksas <takuminakayama4869@gmail.com>
2024-06-23 15:33:08 +0900

    Tile

--- app/src/main/java/net/taksas/apps/watchwaterejecter/tile/MainTileService.kt
+++ app/src/main/java/net/taksas/apps/watchwaterejecter/tile/MainTileService.kt
@@ -1,16 +1,23 @@
 package net.taksas.apps.watchwaterejecter.tile
 
 import android.content.Context
+import android.graphics.Color
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.wear.protolayout.ColorBuilders
 import androidx.wear.protolayout.ColorBuilders.argb
+import androidx.wear.protolayout.DimensionBuilders
 import androidx.wear.protolayout.LayoutElementBuilders
 import androidx.wear.protolayout.ResourceBuilders
 import androidx.wear.protolayout.TimelineBuilders
+import androidx.wear.protolayout.material.Button
+import androidx.wear.protolayout.material.ChipColors
 import androidx.wear.protolayout.material.Colors
 import androidx.wear.protolayout.material.Text
+import androidx.wear.protolayout.material.TitleChip
 import androidx.wear.protolayout.material.Typography
 import androidx.wear.protolayout.material.layouts.PrimaryLayout
 import androidx.wear.tiles.RequestBuilders
@@ -19,6 +26,10 @@ import com.google.android.horologist.annotations.ExperimentalHorologistApi
 import com.google.android.horologist.compose.tools.LayoutRootPreview
 import com.google.android.horologist.compose.tools.buildDeviceParameters
 import com.google.android.horologist.tiles.SuspendingTileService
+import androidx.wear.protolayout.ActionBuilders
+import androidx.wear.protolayout.ModifiersBuilders
+import net.taksas.apps.watchwaterejecter.R
+import net.taksas.apps.watchwaterejecter.presentation.EjectActivity
 
 private const val RESOURCES_VERSION = "0"
 
@@ -48,14 +59,55 @@ class MainTileService : SuspendingTileService() {
     }
 }
 
+
 private fun tileLayout(context: Context): LayoutElementBuilders.LayoutElement {
-    return PrimaryLayout.Builder(buildDeviceParameters(context.resources))
-        .setContent(
-            Text.Builder(context, "Hello World!")
-                .setColor(argb(Colors.DEFAULT.onSurface))
-                .setTypography(Typography.TYPOGRAPHY_CAPTION1)
+    val buttonClickable = ModifiersBuilders.Clickable.Builder()
+        .setId("eject_button")
+        .setOnClick(
+            ActionBuilders.LaunchAction.Builder()
+                .setAndroidActivity(
+                    ActionBuilders.AndroidActivity.Builder()
+                        .setPackageName(context.packageName)
+                        .setClassName(EjectActivity::class.java.name)
+                        .build()
+                )
                 .build()
-        ).build()
+        )
+        .build()
+
+    return PrimaryLayout.Builder(buildDeviceParameters(context.resources))
+        .setPrimaryLabelTextContent(
+            Text.Builder(context,  context.getString(R.string.app_name))
+                .setTypography(Typography.TYPOGRAPHY_CAPTION1)
+                .setColor(ColorBuilders.argb(Color.parseColor("#FF66B3FF")))
+                .build()
+        )
+        .setContent(
+            TitleChip.Builder(context, context.getString(R.string.tile_start_button_label), buttonClickable, buildDeviceParameters(context.resources))
+                // TitleChip/Chip's default width == device width minus some padding
+                // Since PrimaryLayout's content slot already has margin, this leads to clipping
+                // unless we override the width to use the available space
+                .setWidth(DimensionBuilders.ExpandedDimensionProp.Builder().build())
+                .setChipColors(
+                    ChipColors(
+                        /*backgroundColor=*/
+                        argb(Color.parseColor("#FF0033CC")),
+                        /*contentColor=*/
+                        argb(Color.parseColor("#FF99CCFF"))
+                    )
+                )
+                .build()
+        )
+        .setSecondaryLabelTextContent(
+            Text.Builder(context, context.getString(R.string.tile_sub_label))
+                .setTypography(Typography.TYPOGRAPHY_CAPTION2)
+                .setColor(ColorBuilders.argb(Color.parseColor("#FF66B3FF")))
+                .build()
+        )
+        .build()
+
+
+
 }
 
 @Preview(
